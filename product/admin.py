494a35c5dbb8930bac0bef75cc91938293276c84
Oleksii Bodnarchuk <olbody12@gmail.com>
2023-01-27 15:56:06 +0200

    configured admin page for models

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from product.models import User, Category, Product, Order
+
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "get_category", "user", "price"]
+    search_fields = ["price"]
+
+    @admin.display(ordering='category__name', description='Category')
+    def get_category(self, obj):
+        return obj.category.name
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "get_product", "get_category", "get_price"]
+    search_fields = ["product"]
+
+    @admin.display(ordering='product__name', description='Product')
+    def get_product(self, obj):
+        return obj.product.name
+
+    @admin.display(ordering='product__category__name', description='Category')
+    def get_category(self, obj):
+        return obj.product.category.name
+
+    @admin.display(ordering='product__price', description='Price')
+    def get_price(self, obj):
+        return obj.product.price
